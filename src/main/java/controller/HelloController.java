b52af1f40cdc5b2b6b76ab6973b103add63c5e56
dsgwin <dsgwin@gmail.com>
2023-07-10 11:54:34 -0400

    Added JDBC

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.c195;
+package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.c195 {
     requires javafx.fxml;
             
                             
-    opens com.example.c195 to javafx.fxml;
-    exports com.example.c195;
+    opens com.example.C195 to javafx.fxml;
+    exports com.example.C195;
+    exports controller;
+    opens controller to javafx.fxml;
 }
