6cc9de8d7762ea72c33b83507bce49bb704a8fa5
dsgwin <dsgwin@gmail.com>
2023-07-10 22:28:41 -0400

    Added getters and setters for FirstLevelDivisions

--- src/main/java/model/FirstLevelDivisions.java
+++ src/main/java/model/FirstLevelDivisions.java
@@ -1,4 +1,76 @@
 package model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class FirstLevelDivisions {
+    public FirstLevelDivisions(int division_id, String divisionName) {
+        this.division_id = division_id;
+        this.divisionName = divisionName;
+    }
+
+    public int getDivision_id() {
+        return division_id;
+    }
+
+    public void setDivision_id(int division_id) {
+        this.division_id = division_id;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public Timestamp getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Timestamp last_update) {
+        this.last_update = last_update;
+    }
+
+    public String getLast_updated_by() {
+        return last_updated_by;
+    }
+
+    public void setLast_updated_by(String last_updated_by) {
+        this.last_updated_by = last_updated_by;
+    }
+
+    public int getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(int country_id) {
+        this.country_id = country_id;
+    }
+
+    private int division_id;
+    private String divisionName;
+    private Date create_date;
+    private String created_by;
+    private Timestamp last_update;
+    private String last_updated_by;
+    private int country_id;
+
 }
