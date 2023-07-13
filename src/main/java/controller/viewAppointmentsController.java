2bee5236b5395d0c1441a0acd47f12c0a4e37b04
dsgwin <dsgwin@gmail.com>
2023-07-13 10:38:27 -0400

    Modified viewCustomers.fxml and viewAppointments.fxml

--- src/main/java/controller/viewAppointmentsController.java
+++ src/main/java/controller/viewAppointmentsController.java
@@ -1,4 +1,67 @@
 package controller;
 
-public class viewAppointmentsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.ToggleGroup;
+
+public class viewAppointmentsController  {
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TableColumn<?, ?> titleCol;
+
+    @FXML
+    private TableColumn<?, ?> descriptionCol;
+
+    @FXML
+    private TableColumn<?, ?> locationCol;
+
+    @FXML
+    private TableColumn<?, ?> contactCol;
+
+    @FXML
+    private TableColumn<?, ?> typeCol;
+
+    @FXML
+    private TableColumn<?, ?> startCol;
+
+    @FXML
+    private TableColumn<?, ?> endCol;
+
+    @FXML
+    private TableColumn<?, ?> customerIdCol;
+
+    @FXML
+    private TableColumn<?, ?> userIdCol;
+
+    @FXML
+    private Label errorPaneTxt;
+
+    @FXML
+    private ToggleGroup weekMonth;
+
+    @FXML
+    void onAddBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMainMenuBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateBtnClick(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/controller/viewCustomerController.java
+++ src/main/java/controller/viewCustomerController.java
@@ -1,4 +1,57 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
 public class viewCustomerController {
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private TableColumn<?, ?> addressCol;
+
+    @FXML
+    private TableColumn<?, ?> cityCol;
+
+    @FXML
+    private TableColumn<?, ?> stateProvCol;
+
+    @FXML
+    private TableColumn<?, ?> postalCodeCol;
+
+    @FXML
+    private TableColumn<?, ?> countryCol;
+
+    @FXML
+    private TableColumn<?, ?> phoneCol;
+
+    @FXML
+    private Label errorPaneTxt;
+
+    @FXML
+    void onAddBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMainMenuBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateBtnClick(ActionEvent event) {
+
+    }
+
 }
