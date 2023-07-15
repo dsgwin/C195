0eecc9f7ca2d8dfd5face646dbb5dc19927e3db6
Duncan Gwin <dsgwin@gmail.com>
2023-07-15 17:14:56 -0600

    Updated FirstLevelDivisions.java to remove unnecessary attributes and added constructor.

--- src/main/java/model/FirstLevelDivisions.java
+++ src/main/java/model/FirstLevelDivisions.java
@@ -7,67 +7,26 @@ public class FirstLevelDivisions {
 
     private int divisionId;
     private String divisionName;
+    private int countryId;
+
+    public FirstLevelDivisions(int divisionId, String divisionName, int countryId) {
+        this.divisionId = divisionId;
+        this.divisionName = divisionName;
+        this.countryId = countryId;
+    }
 
     public int getDivisionId() {
         return divisionId;
-    }
-
-    public void setDivisionId(int divisionId) {
-        this.divisionId = divisionId;
     }
 
     public String getDivisionName() {
         return divisionName;
     }
 
-    public void setDivisionName(String divisionName) {
-        this.divisionName = divisionName;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
-    }
-
     public int getCountryId() {
         return countryId;
     }
 
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
-    }
 
-    private Date createDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
-    private int countryId;
 
 }
