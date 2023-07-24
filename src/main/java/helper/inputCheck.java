d3a919758f099d031dc52a5072403f0d53d3a7ff
Duncan Gwin <dsgwin@gmail.com>
2023-07-24 16:59:57 -0400

    Added function to check if appointment is between business hours.

--- src/main/java/helper/inputCheck.java
+++ src/main/java/helper/inputCheck.java
@@ -1,5 +1,8 @@
 package helper;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public abstract class inputCheck {
 
     public Boolean customerCheck(){
@@ -7,5 +10,20 @@ public abstract class inputCheck {
 
 
         return successfulCheck;
+    }
+
+    public Boolean businessHoursCheck(LocalDateTime appointmentStart, LocalDateTime appointmentEnd){
+        Boolean betweenBusinessHours = false;
+        LocalTime businessOpen = LocalTime.of(8,00);
+        LocalTime businessClose = LocalTime.of(22, 00);
+
+        LocalTime appointmentStartTime = appointmentStart.toLocalTime();
+        LocalTime appointmentEndTime = appointmentEnd.toLocalTime();
+
+        if((appointmentStartTime.isAfter(businessOpen) && appointmentStartTime.isBefore(businessClose)) && (appointmentEndTime.isAfter(businessOpen) && appointmentEndTime.isBefore(businessClose))){
+            betweenBusinessHours = true;
+        }
+
+        return betweenBusinessHours;
     }
 }
